6138bf324f530c50b185e601fc57d7c6df62ef48
mayank0508 <thakurmayank0508@gmail.com>
2021-07-15 02:15:08 +0530

    API Key 👋

---
+++ src/script.js
@@ -0,0 +1,5 @@
+let weather = {
+    'apiKey': "5941afe941c82840515a5cb204591390"  // i went to open weather.org and then made my account after doing these things i just
+                                                  // went to the option of API Key and fetch the Key from that place.
+ 
+};
